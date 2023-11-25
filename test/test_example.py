ceaa5d0769e1aa2d01751ab7ca0615fc7ab8a7a8
Dragon-Git <1762578117@qq.com>
2023-11-25 17:06:41 +0800

    [uvmgen] add example test

--- test/test_example.py
+++ test/test_example.py
@@ -2,7 +2,7 @@ import pytest
 from pathlib import Path
 from uvmgen.uvmgen import uvm_gen
 
-base_json_paths = Path(__file__).parent.joinpath('json/base_pkg').iterdir()
+base_json_paths = Path(__file__).parent.joinpath('json/example').iterdir()
 @pytest.mark.parametrize("jsonpath", base_json_paths) 
 def test_agt(jsonpath):
-    uvm_gen().gen(jsonpath, "tb/base_pkg")
+    uvm_gen().gen(jsonpath, "tb/example_tb")
