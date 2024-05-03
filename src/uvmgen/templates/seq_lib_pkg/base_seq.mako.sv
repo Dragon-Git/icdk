e9d40ec4239cb54fecdc3346a6ba0789b0649961
Dragon-Git <42856316+Dragon-Git@users.noreply.github.com>
2024-05-03 14:25:43 +0800

    [uvmgen] bug fix for uvm post 1.1

--- src/uvmgen/templates/seq_lib_pkg/base_seq.mako.sv
+++ src/uvmgen/templates/seq_lib_pkg/base_seq.mako.sv
@@ -16,6 +16,9 @@ class ${seq_lib_name}_base_seq extends uvm_sequence #(uvm_sequence_item);
     endtask: pre_body
 
     virtual task post_body();
+        `ifdef UVM_POST_VERSION_1_1
+        var uvm_phase starting_phase = get_starting_phase();
+        `endif
         if(starting_phase != null) starting_phase.drop_objection(this, {get_type_name(), "seq finished"});
     endtask: post_body
 
