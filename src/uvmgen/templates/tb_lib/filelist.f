9aad2fd5d448072cac3305ec8e111211e8bc803e
Dragon-Git <42856316+Dragon-Git@users.noreply.github.com>
2025-02-13 12:51:12 +0800

    close #22

--- src/uvmgen/templates/tb_lib/filelist.f
+++ src/uvmgen/templates/tb_lib/filelist.f
@@ -3,7 +3,7 @@
 $TB_PATH/${pkg}/${pkg.replace("agt_pkg", "if")}.gen.sv
     % endif
     % if "pk_syoscb" in pkg:
-+incdir+$SYOSCB_HOME/uvm_syoscb/src
++incdir+$SYOSCB_HOME/src
 $SYOSCB_HOME/lib/pk_utils_uvm.sv
 $SYOSCB_HOME/src/${pkg}.sv
     % elif "tb" not in pkg:
